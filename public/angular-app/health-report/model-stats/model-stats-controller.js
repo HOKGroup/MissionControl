9f04e2ff77e8a14185e6d0c6747956e8804d9d44
Konrad K Sobon <sobon.konrad@gmail.com>
2018-03-12 09:05:28 -0400

    cleanup

--- public/angular-app/health-report/model-stats/model-stats-controller.js
+++ public/angular-app/health-report/model-stats/model-stats-controller.js
@@ -1,50 +1,30 @@
 angular.module('MissionControlApp').controller('ModelStatsController', ModelStatsController);
 
-function ModelStatsController($routeParams, UtilityService, DTColumnDefBuilder, DTInstances){
+function ModelStatsController($routeParams, UtilityService, DTColumnDefBuilder){
     var vm = this;
     vm.projectId = $routeParams.projectId;
     vm.ModelData = this.processed;
-
     var allData = this.full;
+
     vm.ModelSizes = allData.modelSizes;
     vm.ModelOpenTimes = allData.openTimes;
-
-    var worksetData = {};
-    allData.onOpened.forEach(function (item) {
-        if(!item.user) return;
-
-        if(worksetData.hasOwnProperty(item.user)){
-            worksetData[item.user].push(item);
-        } else {
-            worksetData[item.user] = [item];
-        }
-    });
-
-    vm.modelOpenTimes2 = [];
-    allData.openTimes.forEach(function (item) {
-        if(!item.user) return;
-
-        if(worksetData.hasOwnProperty(item.user)){
-            var data = worksetData[item.user].find(function (date) {
-                return Math.abs(new Date(date.createdOn) - new Date(item.createdOn)) < 10000;
-            });
-            if(data){
-                item['worksets'] = parseFloat((data.opened * 100) / (data.closed + data.opened)).toFixed(0) + '%';
-                item['worksetPercentage'] = (data.opened * 100) / (data.closed + data.opened);
-                vm.modelOpenTimes2.push(item);
-            }
-        } else {
-
-        }
-    });
-
     vm.ModelSynchTimes = allData.synchTimes;
-    vm.selectedUser = "All";
+    vm.selectedSynchUser = "All";
+    vm.selectedOpenUser = "All";
 
-    vm.users = Array.from(new Set(vm.ModelOpenTimes.map(function(item){
+    vm.OpenUsers = Array.from(new Set(vm.ModelOpenTimes.map(function(item){
         return item.user;
     })));
-    vm.users.unshift("All");
+    vm.OpenUsers.unshift("All");
+
+    vm.SynchUsers = Array.from(new Set(vm.ModelSynchTimes.map(function(item){
+        return item.user;
+    })));
+    vm.SynchUsers.unshift("All");
+
+    vm.selectedTableData = 'Synch';
+    vm.TableDataTypes = ['Open', 'Synch'];
+    setTableData(vm.selectedTableData);
 
     vm.dtOptions = {
         paginationType: 'simple_numbers',
@@ -62,23 +42,93 @@ function ModelStatsController($routeParams, UtilityService, DTColumnDefBuilder, 
         DTColumnDefBuilder.newColumnDef(5).notVisible() //workset percentage
     ];
 
-    var dtInstance;
-    DTInstances.getLast().then(function(inst) {
-        dtInstance = inst;
-    });
+    function setTableData(dataType){
+        var worksetData = {};
+        var tempData = [];
+        if(dataType === 'Open'){
+            allData.onOpened.forEach(function (item) {
+                if(!item.user) return;
+
+                if(worksetData.hasOwnProperty(item.user)){
+                    worksetData[item.user].push(item);
+                } else {
+                    worksetData[item.user] = [item];
+                }
+            });
+
+            allData.openTimes.forEach(function (item) {
+                if(!item.user) return;
+
+                if(worksetData.hasOwnProperty(item.user)){
+                    var data = worksetData[item.user].find(function (date) {
+                        return Math.abs(new Date(date.createdOn) - new Date(item.createdOn)) < 10000;
+                    });
+                    if(data){
+                        item['worksets'] = parseFloat((data.opened * 100) / (data.closed + data.opened)).toFixed(0) + '%';
+                        item['worksetPercentage'] = (data.opened * 100) / (data.closed + data.opened);
+                        tempData.push(item);
+                    }
+                }
+            });
+        } else if (dataType === 'Synch'){
+            allData.onSynched.forEach(function (item) {
+                if(!item.user) return;
+
+                if(worksetData.hasOwnProperty(item.user)){
+                    worksetData[item.user].push(item);
+                } else {
+                    worksetData[item.user] = [item];
+                }
+            });
+
+            allData.synchTimes.forEach(function (item) {
+                if(!item.user) return;
+
+                if(worksetData.hasOwnProperty(item.user)){
+                    var data = worksetData[item.user].find(function (date) {
+                        return Math.abs(new Date(date.createdOn) - new Date(item.createdOn)) < 10000;
+                    });
+                    if(data){
+                        item['worksets'] = parseFloat((data.opened * 100) / (data.closed + data.opened)).toFixed(0) + '%';
+                        item['worksetPercentage'] = (data.opened * 100) / (data.closed + data.opened);
+                        tempData.push(item);
+                    }
+                }
+            });
+        }
+
+        vm.tableData = tempData;
+    }
+
+    vm.setTableDataType = function (type) {
+        vm.selectedTableData = type;
+        setTableData(vm.selectedTableData);
+    };
 
     /**
      * Filters Model Open Time data for specific user only.
      * @param user
+     * @param data
      */
-    vm.setUserFilter = function (user) {
-        vm.selectedUser = user;
-        if(user === "All"){
-            vm.ModelOpenTimes = allData.openTimes;
-        } else {
-            vm.ModelOpenTimes = allData.openTimes.filter(function (item) {
-                return item.user === user;
-            });
+    vm.setUserFilter = function (user, data) {
+        if(data === 'synchTimes'){
+            vm.selectedSynchUser = user;
+            if(user === 'All'){
+                vm.ModelSynchTimes = allData.synchTimes;
+            } else {
+                vm.ModelSynchTimes = allData.synchTimes.filter(function (item) {
+                    return item.user === user;
+                });
+            }
+        } else if (data === 'openTimes'){
+            vm.selectedOpenUser = user;
+            if(user === 'All'){
+                vm.ModelOpenTimes = allData.openTimes;
+            } else {
+                vm.ModelOpenTimes = allData.openTimes.filter(function (item) {
+                    return item.user === user;
+                });
+            }
         }
     };
 
