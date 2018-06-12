7afd9ce4aa246aa3a04bfe7604ed732c56620110
ksobon <ksobon1986@gmail.com>
2018-06-12 08:33:29 -0400

    groups prop added

--- app/models/project.js
+++ app/models/project.js
@@ -13,38 +13,39 @@ var geoSchema = new mongoose.Schema({
 },{_id: false});
 
 var projectSchema = new mongoose.Schema({
-    number: String,
-    name: String,
-	office: String,
-	address:{
-		formattedAddress: String,
-		street1: String,
-		street2: String,
-		city: String,
-		state: String,
-		country: String,
-		zipCode: String,
-		placeId: String //google place ID
-	},
-	geoLocation: geoSchema, //type point
-	geoPolygon: geoSchema, //type MultiPolygon
-	configurations: [{
-    	type: mongoose.Schema.Types.ObjectId,
-		ref: 'Configuration' }],
-    triggerRecords: [{ type: mongoose.Schema.Types.ObjectId }],
-    sheets: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Sheets' }],
-	modelStats: [{ type: mongoose.Schema.Types.ObjectId }],
-	linkStats: [{ type: mongoose.Schema.Types.ObjectId }],
-	styleStats: [{ type: mongoose.Schema.Types.ObjectId }],
-	familyStats: [{ type: mongoose.Schema.Types.ObjectId }],
-	worksetStats: [{ type: mongoose.Schema.Types.ObjectId }],
-	viewStats: [{ type: mongoose.Schema.Types.ObjectId }]
-  },
-  {
-	  timestamps: true
-  });
+        number: String,
+        name: String,
+        office: String,
+        address:{
+            formattedAddress: String,
+            street1: String,
+            street2: String,
+            city: String,
+            state: String,
+            country: String,
+            zipCode: String,
+            placeId: String //google place ID
+        },
+        geoLocation: geoSchema, //type point
+        geoPolygon: geoSchema, //type MultiPolygon
+        configurations: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Configuration' }],
+        triggerRecords: [{ type: mongoose.Schema.Types.ObjectId }],
+        sheets: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Sheets' }],
+        modelStats: [{ type: mongoose.Schema.Types.ObjectId }],
+        linkStats: [{ type: mongoose.Schema.Types.ObjectId }],
+        styleStats: [{ type: mongoose.Schema.Types.ObjectId }],
+        familyStats: [{ type: mongoose.Schema.Types.ObjectId }],
+        worksetStats: [{ type: mongoose.Schema.Types.ObjectId }],
+        viewStats: [{ type: mongoose.Schema.Types.ObjectId }],
+        groupStats: [{ type: mongoose.Schema.Types.ObjectId }]
+    },
+    {
+        timestamps: true
+    });
 
 projectSchema.index({ geoLocation: '2dsphere'});
 projectSchema.index({ geoPolygon: '2dsphere'});
