f175cba170370ddf016974bd4062a9f4240f0e80
Konrad K Sobon <sobon.konrad@gmail.com>
2018-11-19 11:18:31 -0500

    Update config.js

--- app/controller/config.js
+++ app/controller/config.js
@@ -1,7 +1,9 @@
 /**
  * Created by konrad.sobon on 2018-02-14.
  */
+
+// (Konrad) Checkout HOK Teams for these credentails. They were reset, and stored under Wiki.
 module.exports = {
-    user: 'DoNotReply@hok.com',
-    host: 'gwsmtp.hok.com'
+    user: '',
+    host: ''
 };
