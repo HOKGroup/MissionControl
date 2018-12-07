6c884db990681817759543052387eb27b20bdd6c
Dan Siroky <dan.siroky@hok.com>
2018-12-07 14:39:44 -0600

    Add optional office query parameters to addins endpoints

--- public/angular-app/data-factory/addins-factory.js
+++ public/angular-app/data-factory/addins-factory.js
@@ -10,11 +10,22 @@ function AddinsFactory($http){
             return $http.get('/api/v2/addins/' + year).then(complete).catch(failed);
         },
 
-        getUsersOfPlugin: function getUsersOfPlugin(name, year){
-            return $http.get('/api/v2/addins/' + year + '?name=' + name).then(complete).catch(failed);
+        getUsersOfPlugin: function getUsersOfPlugin(name, year, office){
+            var officeFilter = '';
+            if (office !== undefined) {
+                var officeCodes = office.reduce(function (accumulator, value) { return accumulator = accumulator + '|' + value });
+                officeFilter += '&office=' + officeCodes;
+            }
+            return $http.get('/api/v2/addins/' + year + '?name=' + name  + officeFilter).then(complete).catch(failed);
         },
-        getAddinManagerDetails: function getAddinManagerDetails(year){
-            return $http.get('/api/v2/addins/' + year  + '/addinmanager').then(complete).catch(failed); 
+
+        getAddinManagerDetails: function getAddinManagerDetails(year, office){
+            var officeFilter = '';
+            if (office !== undefined) {
+                var officeCodes = office.reduce(function (accumulator, value) { return accumulator = accumulator + '|' + value });
+                officeFilter += '?office=' + officeCodes;
+            }
+            return $http.get('/api/v2/addins/' + year  + '/addinmanager' +  officeFilter).then(complete).catch(failed); 
         }
     };
 
