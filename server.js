835373d8cedffba88a26b09a6eb68f4102a286f9
Albhox <albertotono3@gmail.com>
2019-03-30 22:44:52 -0700

    TTCore Hackathon
    Time Series Forecwsting
    Spectacles Viewer
    Logo and Connection
    New Chart coming soon

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 //implemented as a node.js
 //REST API driven mongoDB web server.
 //
-//Copyright 2018 by Jinsol Kim, Konrad K Sobon, HOK
+//Copyright 2018 by Jinsol Kim, Konrad K Sobon, Alberto Tono HOK
 
 var pkg = require('./package.json');
 var cool = require('cool-ascii-faces');
