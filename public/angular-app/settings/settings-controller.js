fe3d00ea03a7504d5239c6d6a7f778f67ab8e96b
ksobon <sobon.konrad@gmail.com>
2019-05-02 15:16:01 -0400

    add settings page + controller

---
+++ public/angular-app/settings/settings-controller.js
@@ -0,0 +1,8 @@
+/**
+ * Created by konrad.sobon on 2019-05-02.
+ */
+angular.module('MissionControlApp').controller('SettingsController', SettingsController);
+
+function SettingsController(){
+    var vm = this;
+}
